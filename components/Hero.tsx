45e0416291bf38bd970cd16d0d18957fafb26053
Vader1970 <daniel.wilkey@gmail.com>
2024-04-18 19:02:40 +1200

    Fix overlay in hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
     <div className='hero'>
       <div className='flex-1 pt-36 padding-x'>
         {/* Hero title */}
-        <h1 className='hero__title'>Find, book, or rent a car — quickly and easily!</h1>
+        <h1 className='hero__title'>Find, book, or rent a car — quickly and super easily!</h1>
 
         {/* Hero subtitle */}
         <p className='hero__subtitle'>Streamline your car rental experiance with our effortless booking process.</p>
@@ -35,10 +35,10 @@ const Hero = () => {
         <div className='hero__image'>
           {/* Display the hero image */}
           <Image src='/hero.png' alt='hero' fill className='object-contain' />
-
-          {/* Overlay for the hero image */}
-          <div className='hero__image-overlay' />
         </div>
+
+        {/* Overlay for the hero image */}
+        <div className='hero__image-overlay' />
       </div>
     </div>
   );
